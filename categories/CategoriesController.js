9483993a664a6ef6ec83399d9a3f7e7ec8077eac
Mateus Martinez rosa <41523047+mateusmrosa@users.noreply.github.com>
2021-01-23 18:39:05 -0300

    Primeira carga

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -7,20 +7,52 @@ router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new");
 })
 
+//salva no banco de dados
 router.post("/categories/save", (req, res) => {
     var title = req.body.title;
-
-    if (title != undefined) {
+    if (title != "") {
         Category.create({
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
     } else {
         res.redirect("/admin/categories/new")
     }
 })
 
+//lista as categorias
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {
+            categories: categories
+        });
+    });
+
+});
+
+
+//deleter categorias
+router.post("/categories/delete", (req, res) => {
+
+    var id = req.body.id;
+    console.log(id);
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            })
+        } else
+            res.redirect("/admin/categories");
+    }
+    else
+        res.redirect("/admin/categories");
+
+});
 
 module.exports = router;
